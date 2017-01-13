479b1247a8a229cad700299f3ca4d901942177f3
Hung Tran <hung@0x0.ninja>
2017-01-12 18:55:30 -0600

    remove bower

--- assets.js
+++ assets.js
@@ -1,22 +1,22 @@
 export default {
   js: [
-    'static/assets/bower/jquery/dist/jquery.min.js',
-    'static/assets/bower/materialize/dist/js/materialize.min.js',
-    'static/assets/bower/handlebars/handlebars.min.js',
-    'static/assets/bower/blueimp-file-upload/js/vendor/jquery.ui.widget.js',
-    'static/assets/bower/blueimp-file-upload/js/jquery.iframe-transport.js',
-    'static/assets/bower/blueimp-file-upload/js/jquery.fileupload.js',
-    'static/assets/bower/blueimp-file-upload/js/jquery.fileupload-process.js',
-    'static/assets/bower/clipboard/dist/clipboard.min.js',
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/materialize-css/dist/js/materialize.min.js',
+    'node_modules/handlebars/dist/handlebars.min.js',
+    'node_modules/blueimp-file-upload/js/vendor/jquery.ui.widget.js',
+    'node_modules/blueimp-file-upload/js/jquery.iframe-transport.js',
+    'node_modules/blueimp-file-upload/js/jquery.fileupload.js',
+    'node_modules/blueimp-file-upload/js/jquery.fileupload-process.js',
+    'node_modules/clipboard/dist/clipboard.min.js',
     'static/assets/js/jquery.fileupload-validate.js',
     'static/assets/js/main.js'
   ],
   css: [
-    'static/assets/bower/materialize/dist/css/materialize.min.css',
+    'node_modules/materialize-css/dist/css/materialize.min.css',
     'static/assets/css/main.css'
   ],
   fonts: [
-    'static/assets/bower/materialize/fonts/**/*',
+    'node_modules/materialize-css/dist/fonts/**/*',
     'static/assets/fonts/MaterialIcons.woff2'
   ]
 }
