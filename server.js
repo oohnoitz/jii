9e881d6e08af12588f2852c919439873eafa5c16
Hung Tran <hung@0x0.ninja>
2017-01-13 18:18:11 -0600

    update server to use env variables for HOST and PORT

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import app from './lib/express'
 import config from './config'
 
 const debug = require('debug')('jii::server')
+const host = process.env.HOST || config.app.host
+const port = process.env.PORT || config.app.port
 
-app.listen(config.app.port, () => {
+app.listen(port, host, () => {
   debug(`started server at: ${config.app.host}:${config.app.port}`)
 })
