5f45131e34b4b34b7991f78a2409fe3a5f264b43
Hung Tran <hung@0x0.ninja>
2017-03-14 19:26:20 -0500

    allow disabling web ui and moving file serve endpoint to /r/

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import jiiData from '../lib/jii'
+import config from '../config'
 
 const routes = express.Router()
 
@@ -11,13 +12,17 @@ routes.get('/api/v1/delete/:guid/:hash', apiFilesController.remove)
 routes.get('/api/v1/upload/:guid', apiFilesController.select)
 
 import pagesController from './controllers/pages'
-routes.get('/about', pagesController.about)
-routes.get('/terms-of-service', pagesController.terms)
-routes.get('/privacy-policy', pagesController.privacy)
-routes.get('/', pagesController.index)
+if (config.web.ui) {
+  routes.get('/about', pagesController.about)
+  routes.get('/terms-of-service', pagesController.terms)
+  routes.get('/privacy-policy', pagesController.privacy)
+  routes.get('/', pagesController.index)
+} else {
+  routes.get('/', (req, res) => res.end())
+}
 
 import filesController from './controllers/files'
 routes.get('/s/:guid/:algorithm/:passphrase', filesController.serveFile)
-routes.get('/:guid', filesController.serveFile)
+routes.get('/r/:guid', filesController.serveFile)
 
 export default routes
